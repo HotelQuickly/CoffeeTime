6664104440fed5d2c4bef596a0141754496160b3
Josef Nevoral <josef.nevoral@gmail.com>
2015-04-27 16:45:00 +0700

    Change time for meeting

--- app/providers/calendar.js
+++ app/providers/calendar.js
@@ -8,8 +8,8 @@ var debug = require('debug')('coffee:providers:calendar'),
     providers
 
 // todo: define the times from configuration
-var startDateTime = moment().add(2, 'days').hours(7).minutes(0).seconds(0).utcOffset(7),
-    endDateTime = moment().add(2, 'days').hours(7).minutes(30).seconds(0).utcOffset(7)
+var startDateTime = moment().add(2, 'days').hours(9).minutes(0).seconds(0).utcOffset(7),
+    endDateTime = moment().add(2, 'days').hours(9).minutes(30).seconds(0).utcOffset(7)
 
 
 var filterUserCompanyCalendar = function(calendars, userEmail) {
