5916046551ca315ed722df2044fd91e99cbd29e4
Josef Nevoral <josef.nevoral@gmail.com>
2017-07-07 10:34:44 +0200

    Safely load ADMINs and don't creash when ENV is missing

--- config/environment.js
+++ config/environment.js
@@ -9,7 +9,7 @@ module.exports = (function() {
         consumerSecret: process.env.GOOGLE_CONSUMER_SECRET,
         mongoUri: process.env.MONGO_URI,
         eventOrganiserEmail: process.env.EVENT_ORGANISER_EMAIL,
-        admins: process.env.ADMINS.split(','),
+        admins: process.env.ADMINS ? process.env.ADMINS.split(',') : [],
         planForDaysInAdvance: process.env.PLAN_FOR_DAYS_IN_ADVANCE || 2,
         utcOffset: process.env.UTC_OFFSET || 7,
         planToStartHour: process.env.PLAN_TO_START_HOUR || 15,
